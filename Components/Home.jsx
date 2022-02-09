b726643a4470480624bb7509e539b4d423bf169a
Elad Cohen <83339968+EladC921@users.noreply.github.com>
2022-02-09 19:48:40 +0200

    Tasks & Profile
    -added some Tasks Components
    -added a template for Profiel Page
    -added a method that decides the color of each group uniqely

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -7,63 +7,131 @@ import {
   ScrollView,
 } from "react-native";
 import React from "react";
+import Task from "./Tasks/Task";
+import GroupTask from "./Tasks/GroupTask";
 
-const toDoList = [
+let toDoList = [
   {
-    id: 1,
+    id: 0,
+    gid: 1,
+    tid: 1,
+    groupName: "Ruppin Computer Science",
     title: "Learn React Native 1",
     completed: false,
     text: "Learn React Native Now",
+    createdAt: new Date(),
   },
   {
-    id: 2,
+    id: 1,
+    gid: 1,
+    tid: 2,
+    groupName: "Ruppin Computer Science",
     title: "Learn React Native 2",
     completed: false,
     text: "Learn React Native Now!",
+    createdAt: new Date(),
   },
   {
-    id: 3,
+    id: 2,
+    gid: 2,
+    tid: 3,
+    groupName: "Future Factories",
     title: "Learn React Native 3",
     completed: false,
     text: "Learn React Native Now!!",
+    createdAt: new Date(),
   },
   {
-    id: 4,
+    id: 3,
+    gid: 3,
+    tid: 4,
+    groupName: "Cohen Fam",
     title: "Learn React Native 4",
     completed: false,
     text: "Learn React Native Now!!!",
+    createdAt: new Date(),
   },
 ];
 
+let colors = [
+  "rgba(106, 242, 167, 0.5)",
+  "rgba(251, 255, 68, 0.5)",
+  "rgba(107, 240, 255, 0.5)",
+  "rgba(247, 98, 232, 0.5)",
+  "rgba(255, 25, 25, 0.5)",
+  "rgba(79, 255, 87, 0.5)",
+  "rgba(249, 167, 67, 0.5)",
+];
+
+// generate random color for list item
+const getRandomColor = () => {
+  let random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+};
+
+let toDoListGids = [...new Set(toDoList.map((item) => item.gid))];
+toDoListGids = toDoListGids.map((item) => {
+  let color = getRandomColor();
+  colors = colors.filter((c) => c !== color);
+  return { gid: item, color: color };
+});
+
+const getColor = (gid) => {
+  let color = "";
+  toDoListGids.map((item) => {
+    if (item.gid === gid) {
+      color = item.color;
+    }
+  });
+  return color;
+};
+
 const Home = () => {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        <Text style={{ color: "#fff", fontSize: 20 }}>Welcome toBee!</Text>
+        <Text style={{ color: "black", fontSize: 20 }}>Welcome toBee!</Text>
       </View>
-      <FlatList
-        horizontal
-        data={toDoList}
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.list_container}
-        renderItem={({ item }) => (
-          <View style={styles.list_item}>
-            <Text>{item.text}</Text>
-          </View>
-        )}
-      />
-      <FlatList
-        horizontal
-        data={toDoList}
-        contentContainerStyle={styles.list_container}
-        showsHorizontalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <View style={styles.list_item}>
-            <Text>{item.text}</Text>
-          </View>
-        )}
-      />
-    </View>
+      {/** Registerable Tasks */}
+      <View style={{ flex: 3 }}>
+        <Text style={{ position: "absolute", top: 0, left: 5 }}>
+          Registerable Tasks
+        </Text>
+        <FlatList
+          horizontal
+          data={toDoList}
+          contentContainerStyle={styles.list_container}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <Task
+              title={item.title}
+              text={item.text}
+              createdAt={item.createdAt}
+            />
+          )}
+        />
+      </View>
+
+      {/** My Tasks */}
+      <View style={{ flex: 4 }}>
+        <Text style={{ position: "absolute", top: 50, left: 5 }}>My Tasks</Text>
+        <FlatList
+          horizontal
+          data={toDoList}
+          contentContainerStyle={styles.list_container}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <GroupTask
+              color={getColor(item.gid)}
+              groupName={item.groupName}
+              title={item.title}
+              text={item.text}
+              createdAt={item.createdAt}
+            />
+          )}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -77,8 +145,7 @@ const styles = StyleSheet.create({
   },
 
   header: {
-    flex: 0.8,
-    backgroundColor: "#00bfff",
+    flex: 0.5,
     alignItems: "center",
     justifyContent: "center",
     width: "100%",
@@ -87,16 +154,6 @@ const styles = StyleSheet.create({
   list_container: {
     justifyContent: "center",
     alignItems: "center",
-  },
-
-  list_item: {
-    alignItems: "center",
-    justifyContent: "center",
-    margin: 5,
-    padding: 5,
-    backgroundColor: "gray",
-    height: 150,
-    borderRadius: 40,
   },
 });
 
--- Components/Profile.jsx
+++ Components/Profile.jsx
@@ -1,12 +1,103 @@
-import { View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  Button,
+} from "react-native";
 import React from "react";
 
 const Profile = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      {/** Header */}
+      <View style={styles.header}>
+        <Text style={styles.header_Text}> @JimmyNewton_012 </Text>
+      </View>
+      {/** Body */}
+      <View style={styles.body_container}>
+        {/** Profile Data */}
+        <View style={styles.profile_data_container}>
+          {/** Profile Picutre */}
+          <View style={styles.profilePic_container}>
+            <Image
+              source={require("../Images/bee.png")}
+              style={styles.profilePic}
+            />
+          </View>
+          {/** Profile Name */}
+          <View style={styles.profileName_container}>
+            <Text> Jimmy Newton </Text>
+            <Button title="Edit Profile" />
+          </View>
+        </View>
+        {/** My - ToDo */}
+        <View style={{ height: "70%", backgroundColor: "#fff" }}>
+          
+        </View>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#fff",
+  },
+
+  header: {
+    backgroundColor: "black",
+    height: "15%",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+
+  header_Text: {
+    top: "10%",
+    color: "white",
+    fontSize: 20,
+  },
+
+  body_container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#fff",
+  },
+
+  profile_data_container: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    height: "30%",
+    backgroundColor: "rgba(52, 52, 52, 0.2)",
+  },
+
+  profilePic: {
+    resizeMode: "contain",
+    height: 100,
+    width: 100,
+    borderWidth: 5,
+    borderColor: "black",
+    borderRadius: 50,
+  },
+
+  profilePic_container: {
+    margin: 10,
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 5,
+    shadowOpacity: 0.2,
+  },
+
+  profileName_container: {
+    height: 100,
+    justifyContent: "center",
+    margin: 20,
+  },
+});
 
 export default Profile;
