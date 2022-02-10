b41f228e509044f562297a252a23128a476fda7d
Chen <74059786+Dodchen@users.noreply.github.com>
2022-02-10 17:05:31 +0200

    firebase & login
    Adding firebase to the login page for auth.

--- Components/Login.jsx
+++ Components/Login.jsx
@@ -1,156 +1,273 @@
-import { StyleSheet, SafeAreaView, KeyboardAvoidingView, View, Text, TextInput, Modal, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, Pressable, Alert, SafeAreaView, KeyboardAvoidingView, View, Text, TextInput, Modal, Image, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+import { auth } from '../db/firebaseSDK';
 
 
 
 
 const Login = ({ navigation }) => {
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user; 
+        console.log('Logged in with:', user.email);
+        
+      })
+      .catch(error => alert(error.message))
+  }
+  const handleSignUp = () => {
+   
+    auth
+      .createUserWithEmailAndPassword(signmail, csignpass)
+      .then (userCredentials => {
+        const user = userCredentials.user;
+        setOpenModal(false)
+        console.log(user.email);
+        
+      })
+      .catch(error => alert(error.message))
+  }
+  const [signmail, setSignupmail] = useState()
+  const [csignpass, setcpass] = useState()
+  const [signpass, setsignpass ] = useState()
+  const [password, setPassword] = useState()
+  const [email, setEmail] = useState()
   const [openModal, setOpenModal] = useState(false)
   return (
     <>
-      <SafeAreaView style={styles.main}>
-        <View style={styles.header}>
-          <View style={styles.logo}>
+       <View style={styles.container}>
+         
+       <View style={styles.logophoto}>
             <Image
               source={require('../image/logo.png')}
-              style={{ width: 120, height: 120 }} />
+              style={styles.images} />
           </View>
+        <Text style={styles.logo}>toBee</Text>
+        <View style={styles.inputView} >
+          <TextInput  
+            
+            style={styles.inputText}
+            placeholder="Email..." 
+            placeholderTextColor="white"
+            onChangeText={text => setEmail(text)}/>
         </View>
-
-
-
-
-
-        <View style={styles.container}>
-
-          <TextInput style={styles.input}
-            placeholder="Enter Email"
-            type='email' />
-
-          <TextInput style={styles.input}
-            placeholder="Enter password"
-            secureTextEntry />
-          <View style={styles.buttons}>
-            <TouchableOpacity style={styles.btn}>
-              <Text style={styles.lgn_btn}>Login</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={() => setOpenModal(true)} style={styles.btn}>
-              <Text style={styles.lgn_btn}>Register</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={styles.inputView} >
+          <TextInput  
+            
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password..." 
+            placeholderTextColor="white"
+            onChangeText={text => setPassword(text)}/>
         </View>
-
-      </SafeAreaView>
-      {openModal && <Modal><View style={styles.modalPart}>
-        <View style={styles.reg_logo}>
-          <Image
-            source={require('../image/logo.png')}
-            style={{ width: 120, height: 120 }} />
+        
+        <TouchableOpacity onPress={()=> {handleLogin()}} style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setOpenModal(true)}>
+          <Text style={styles.loginText}>Signup</Text>
+        </TouchableOpacity> 
+        {openModal&&
+    <View style={styles.centeredView}>
+    <Modal
+      animationType="slide"
+      transparent={true}
+    >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+        <TouchableOpacity style={styles.xbut} onPress={() => setOpenModal(false)}>
+          <Text>X</Text>
+        </TouchableOpacity>
+        <Text style={styles.logo_reg}>Register</Text>
+        <View style={styles.inputView} >
+          <TextInput  
+          
+            style={styles.inputText}
+            placeholder="Enter Full Name..." 
+            placeholderTextColor="white"
+           />
         </View>
-
-
-        <TextInput style={styles.input}
-          placeholder="Enter Email"
-          type='email' />
-
-        <TextInput style={styles.input}
-          placeholder="Enter password"
-          secureTextEntry />
-        <View style={styles.buttons}>
-          <TouchableOpacity style={styles.btn}>
-            <Text style={styles.lgn_btn}>Login</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={() => setOpenModal(false)} style={styles.btn}>
-            <Text style={styles.lgn_btn}>Register</Text>
-          </TouchableOpacity>
+        <View style={styles.inputView} >
+          <TextInput  
+          
+            style={styles.inputText}
+            placeholder="Enter Nickname..." 
+            placeholderTextColor="white"
+            />
         </View>
-
-      </View></Modal>}
+        <View style={styles.inputView} >
+          <TextInput  
+          
+            style={styles.inputText}
+            placeholder="Enter Email..." 
+            placeholderTextColor="white"
+            onChangeText={text => setSignupmail(text)}/>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput  
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Enter Password..." 
+            placeholderTextColor="white"
+            onChangeText={text => setsignpass(text)}/>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput  
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Confirm Password..." 
+            placeholderTextColor="white"
+            onChangeText={text => setcpass(text)}/>
+        </View>
+        <TouchableOpacity onPress={()=> {handleSignUp()}}  style={styles.loginBtn}>
+          <Text style={styles.loginText}>Register</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+    </View>
+   }
+      </View>
+ 
     </>
   );
 };
 
 const styles = StyleSheet.create({
-
-  main: {
-
-    height: 200,
-    width: "100%",
-    flex: 1,
-    backgroundColor: "gray"
-
-  },
-  header: {
-
-    flex: 1,
-
-  },
   container: {
-
-    flex: 2,
-
-    borderRadius: 30,
+    flex: 1,
+    backgroundColor: '#FFE889',
     alignItems: 'center',
-
+    justifyContent: 'center',
+  },
+  images:{
+    marginBottom:40,
+    width:160,
+    height:185
+  },
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#B35A3F",
+    marginBottom:40
+  },
+  logo_reg:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#B35A3F",
+    marginBottom:10
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#B35A3F",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20,
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  
+  forgot:{
+    color:"white",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"50%",
+    backgroundColor:"#B35A3F",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10,
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  loginText:{
+    color:"white"
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    height:'70%',
+    width:'100%',
+    margin: 20,
+    flex:1,
     backgroundColor: "white",
-    paddingHorizontal: 10
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
   },
-
-  logo: {
-
-    alignItems: 'center',
-    height: '30%',
-  },
-  reg_logo: {
-
-    alignItems: 'center',
-    height: '30%',
-  },
-
-  input: {
+  button: {
+    borderRadius: 20,
     padding: 10,
-    paddingHorizontal: 25,
-    margin: 10,
-    width: 300,
-    backgroundColor: 'gray',
-    borderRadius: 30,
-
-
+    elevation: 2
   },
-  buttons: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-
+  buttonOpen: {
+    backgroundColor: "#F194FF",
   },
-
-  btn: {
-    textAlign: 'center',
-    fontSize: 20,
-    borderRadius: 40,
-    alignItems: 'center',
-    backgroundColor: "black",
-    width: 160,
-    height: 40,
-    marginTop: 34
+  buttonClose: {
+    backgroundColor: "#2196F3",
   },
-  lgn_btn: {
-    paddingTop: 5,
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
   },
-
-  modalPart: {
-
-    borderRadius: 30,
-    alignItems: 'center',
-    height: '100%',
-    backgroundColor: "gray",
-    paddingHorizontal: 10
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   },
+  xbut:{
+    borderWidth:2,
+    backgroundColor:'#B35A3F',
+    borderColor:'#B35A3F',
+    borderRadius:10,
+    alignItems:'center',
+    paddingTop:5,
+    height:30,
+    width:30,
+    marginRight:340,
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+    
+
+  }
 })
 
 export default Login;
