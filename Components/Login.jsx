c6b4c046a455cc2ffa8b5a56b1d9f2296f0ad870
Chen <74059786+Dodchen@users.noreply.github.com>
2022-02-11 16:09:46 +0200

    creating Register component

--- Components/Login.jsx
+++ Components/Login.jsx
@@ -2,6 +2,9 @@ import { StyleSheet, Pressable, Alert, SafeAreaView, KeyboardAvoidingView, View,
 import React, { useState } from "react";
 import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 import { auth } from '../db/firebaseSDK';
+import RegisterModal from "./RegisterModal";
+import { CheckBox } from "react-native-elements/dist/checkbox/CheckBox";
+
 
 
 
@@ -10,130 +13,71 @@ const Login = ({ navigation }) => {
   const handleLogin = () => {
     auth
       .signInWithEmailAndPassword(email, password)
+      
       .then(userCredentials => {
-        const user = userCredentials.user; 
+        
         navigation.navigate("Main");
         console.log('Logged in with:', user.email);
       })
+      
       .catch(error => alert(error.message))
-  }
-  const handleSignUp = () => {
-   
-    auth
-      .createUserWithEmailAndPassword(signmail, csignpass)
-      .then (userCredentials => {
-        const user = userCredentials.user;
-        setOpenModal(false)
-        console.log(user.email);
-        
-      })
-      .catch(error => alert(error.message))
+     
   }
   
+  const [isSelected, setSelection] = useState(false)
   const [signmail, setSignupmail] = useState()
   const [csignpass, setcpass] = useState()
-  const [signpass, setsignpass ] = useState()
+  const [signpass, setsignpass] = useState()
   const [password, setPassword] = useState()
   const [email, setEmail] = useState()
   const [openModal, setOpenModal] = useState(false)
 
   return (
-    
+
     <>
-       <View style={styles.container}>
-         
-       <View style={styles.logophoto}>
-            <Image
-              source={require('../image/logo.png')}
-              style={styles.images} />
+
+      <View style={styles.container}>
+      <KeyboardAvoidingView
+          style={styles.container}
+          behavior="padding"
+        >
+        <View style={styles.logophoto}>
+          <Image
+            source={require('../image/logo.png')}
+            style={styles.images} />
+        </View>
+       
+          <Text style={styles.logo}>toBee</Text>
+
+          <View style={styles.inputView} >
+            <TextInput
+
+              style={styles.inputText}
+              placeholder="Email..."
+              placeholderTextColor="white"
+              onChangeText={text => setEmail(text)} />
           </View>
-        <Text style={styles.logo}>toBee</Text>
-        <View style={styles.inputView} >
-          <TextInput  
-            
-            style={styles.inputText}
-            placeholder="Email..." 
-            placeholderTextColor="white"
-            onChangeText={text => setEmail(text)}/>
-        </View>
-        <View style={styles.inputView} >
-          <TextInput  
-            
-            secureTextEntry
-            style={styles.inputText}
-            placeholder="Password..." 
-            placeholderTextColor="white"
-            onChangeText={text => setPassword(text)}/>
-        </View>
-        
-        <TouchableOpacity onPress={()=> {handleLogin()}} style={styles.loginBtn}>
-          <Text style={styles.loginText}>LOGIN</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => setOpenModal(true)}>
-          <Text style={styles.loginText}>Signup</Text>
-        </TouchableOpacity> 
-        {openModal&&
-    <View style={styles.centeredView}>
-    <Modal
-      animationType="slide"
-      transparent={true}
-    >
-      <View style={styles.centeredView}>
-        <View style={styles.modalView}>
-        <TouchableOpacity style={styles.xbut} onPress={() => setOpenModal(false)}>
-          <Text>X</Text>
-        </TouchableOpacity>
-        <Text style={styles.logo_reg}>Register</Text>
-        <View style={styles.inputView} >
-          <TextInput  
-          
-            style={styles.inputText}
-            placeholder="Enter Full Name..." 
-            placeholderTextColor="white"
-           />
-        </View>
-        <View style={styles.inputView} >
-          <TextInput  
-          
-            style={styles.inputText}
-            placeholder="Enter Nickname..." 
-            placeholderTextColor="white"
-            />
-        </View>
-        <View style={styles.inputView} >
-          <TextInput  
-          
-            style={styles.inputText}
-            placeholder="Enter Email..." 
-            placeholderTextColor="white"
-            onChangeText={text => setSignupmail(text)}/>
-        </View>
-        <View style={styles.inputView} >
-          <TextInput  
-            secureTextEntry
-            style={styles.inputText}
-            placeholder="Enter Password..." 
-            placeholderTextColor="white"
-            onChangeText={text => setsignpass(text)}/>
-        </View>
-        <View style={styles.inputView} >
-          <TextInput  
-            secureTextEntry
-            style={styles.inputText}
-            placeholder="Confirm Password..." 
-            placeholderTextColor="white"
-            onChangeText={text => setcpass(text)}/>
-        </View>
-        <TouchableOpacity onPress={()=> {handleSignUp()}}  style={styles.loginBtn}>
-          <Text style={styles.loginText}>Register</Text>
-        </TouchableOpacity>
-        </View>
+          <View style={styles.inputView} >
+            <TextInput
+              secureTextEntry
+              style={styles.inputText}
+              placeholder="Password..."
+              placeholderTextColor="white"
+              onChangeText={text => setPassword(text)} />
+          </View>
+
+          <TouchableOpacity onPress={() => { handleLogin() }} style={styles.loginBtn}>
+            <Text style={styles.loginText}>LOGIN</Text>
+          </TouchableOpacity>
+        <CheckBox
+          disabled={false}
+          value={isSelected}
+          onValueChange={(newValue) => setSelection(newValue)}
+          />
+          <RegisterModal />
+        </KeyboardAvoidingView>
       </View>
-    </Modal>
-    </View>
-   }
-      </View>
- 
+
     </>
   );
 };
@@ -144,32 +88,35 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFE889',
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
   },
-  images:{
-    marginBottom:40,
-    width:160,
-    height:185
+  images: {
+    marginBottom: 40,
+    width: 160,
+    height: 185
   },
-  logo:{
-    fontWeight:"bold",
-    fontSize:50,
-    color:"#B35A3F",
-    marginBottom:40
+  logo: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#B35A3F",
+    marginBottom: 40
   },
-  logo_reg:{
-    fontWeight:"bold",
-    fontSize:50,
-    color:"#B35A3F",
-    marginBottom:10
+  logo_reg: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#B35A3F",
+    marginBottom: 10
   },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#B35A3F",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20,
+  inputView: {
+    width: "80%",
+    backgroundColor: "#B35A3F",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
     shadowOffset: {
       width: 0,
       height: 2
@@ -178,24 +125,24 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5
   },
-  inputText:{
-    height:50,
-    color:"white"
+  inputText: {
+    height: 50,
+    color: "white"
   },
-  
-  forgot:{
-    color:"white",
-    fontSize:11
+
+  forgot: {
+    color: "white",
+    fontSize: 11
   },
-  loginBtn:{
-    width:"50%",
-    backgroundColor:"#B35A3F",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10,
+  loginBtn: {
+    width: "50%",
+    backgroundColor: "#B35A3F",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
     shadowOffset: {
       width: 0,
       height: 2
@@ -204,8 +151,8 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5
   },
-  loginText:{
-    color:"white"
+  loginText: {
+    color: "white"
   },
   centeredView: {
     flex: 1,
@@ -213,24 +160,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: 22
   },
-  modalView: {
-    height:'70%',
-    width:'100%',
-    margin: 20,
-    flex:1,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
-  },
+ 
   button: {
     borderRadius: 20,
     padding: 10,
@@ -247,30 +177,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center"
   },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center"
-  },
-  xbut:{
-    borderWidth:2,
-    backgroundColor:'#B35A3F',
-    borderColor:'#B35A3F',
-    borderRadius:10,
-    alignItems:'center',
-    paddingTop:5,
-    height:30,
-    width:30,
-    marginRight:340,
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
-    
-
-  }
+ 
 })
 
 export default Login;
