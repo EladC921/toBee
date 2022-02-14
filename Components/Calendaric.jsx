64287ef86aadbe26915f7e63dd18506e87d8a9e5
Chen <74059786+Dodchen@users.noreply.github.com>
2022-02-14 10:25:58 +0200

    adding basic calendar options

--- Components/Calendaric.jsx
+++ Components/Calendaric.jsx
@@ -1,44 +1,58 @@
-import { View, Text,StyleSheet } from "react-native";
-import React from "react";
-import { Calendar,CalendarList, Agenda } from "react-native-calendars";
+import { View,time, Text, StyleSheet,timeToString,TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 
 
 /**test */
 const Calendaric = () => {
+  const vacation = {key:'vacation', color: 'red', selectedDotColor: 'blue'};
+const massage = {key:'massage', color: 'blue', selectedDotColor: 'blue'};
+const workout = {key:'workout', color: 'green'};
+
   return (
     <View style={styles.page}>
-     <View style={styles.container}>
-     <CalendarList
-     horizontal={true}
-     style={styles.cal}
-     />
-     </View>
       
+      <View style={styles.container}>
+
+      <Calendar
+      
+      markingType={'multi-dot'}
+      markedDates={{
+        '2022-02-25': {dots: [vacation, massage, workout], selected: true, selectedColor: 'red'},
+        '2022-02-26': {dots: [massage, workout], disabled: true}
+      }}
+       onDayPress={day => {
+        alert(day.dateString);
+      }}
+      />
+      </View>
+
     </View>
   );
 };
 const styles = StyleSheet.create({
-  page:{
-    flex:1
+  page: {
+    flex: 1
   },
   container: {
-    flex: 1,
-    backgroundColor: '#FFE889',
     
+    backgroundColor: '#FFE889',
+    height:'100%',
+    flex:1,
     paddingTop:150,
-    width: '100%',   
+    width: '100%',
     shadowOffset: {
       width: 0,
       height: 2
+    },
+    shadowOpacity: 0.8,
+    shadowRadius: 4,
+    elevation: 5
+
   },
-  shadowOpacity: 0.8,
-  shadowRadius: 4,
-  elevation: 5
-  
-  },
-  cal:{
-  borderTopStartRadius:20,
-  borderTopEndRadius:20,
+  cal: {
+    borderTopStartRadius: 20,
+    borderTopEndRadius: 20,
 
 
   },
